c673891b9c1ee1587a757eb70274e0ee2002081d
Asan Sabir <asansaber123321@gmail.com>
2026-08-26 15:59:09 +0300

    Added Role based authentication for registering a user so only admin role can register other users

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const authRouter = Router();
 const authController = require("../controller/authController.js");
+const { authenticateUser, requiredRole } = require("../utils/authMiddleware.js");
+const { ROLE } = require("../utils/enum.js");
 
-authRouter.post("/register", authController.postRegister);
+authRouter.post("/register", authenticateUser, requiredRole(ROLE.ADMIN), authController.postRegister);
 authRouter.post("/login", authController.postLogin);
 authRouter.post("/refresh", authController.postRefresh);
 authRouter.post("/logout", authController.postLogout);
